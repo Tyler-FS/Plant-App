7425a68e4dd45f66c3b8bf639f64967645ac7335
Tyler-FS <Tyler.FoldenSnyder@gmail.com>
2024-11-21 16:06:27 -0800

    removed @Data annotation and used safe annotations to avoid memory and performance issues with Entities (Plant and Room)

--- src/main/java/care/plant/plantappspring/model/Plant.java
+++ src/main/java/care/plant/plantappspring/model/Plant.java
@@ -1,11 +1,16 @@
 package care.plant.plantappspring.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Plant {
 
@@ -28,5 +33,21 @@ public class Plant {
         this.waterFrequency = waterFrequency;
         this.sunlightNeeds = sunlightNeeds;
         this.notes = notes;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Plant plant = (Plant) o;
+        return getId() != null && Objects.equals(getId(), plant.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
--- src/main/java/care/plant/plantappspring/model/Room.java
+++ src/main/java/care/plant/plantappspring/model/Room.java
@@ -1,11 +1,16 @@
 package care.plant.plantappspring.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Room {
 
@@ -30,5 +35,21 @@ public class Room {
         this.windowFacing = windowFacing;
         this.temperature = temperature;
         this.notes = notes;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Room room = (Room) o;
+        return getId() != null && Objects.equals(getId(), room.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
